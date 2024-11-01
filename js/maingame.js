c59a93f9f6ac3543d59e6d891fb5ffb18d901e49
Manu TEAO <mrgeymeurt@gmail.com>
2024-11-01 21:06:01 +0100

    Update maingame.js

--- js/maingame.js
+++ js/maingame.js
@@ -2,14 +2,14 @@ function collectStar (player, star)
 {
     star.disableBody(true, true);
     
-    //  Add and update the score
+    //score
     score += 10;
     $('#score').html("Score : "+score);
-    //scoreText.setText('Score: ' + score);
     
+    //création étoiles
     if (stars.countActive(true) === 0)
     {
-        //  A new batch of stars to collect
+        
         stars.children.iterate(function (child) {
             
             child.enableBody(true, child.x, 0, true, true);
@@ -18,34 +18,15 @@ function collectStar (player, star)
         
         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
         
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-        
     }
-}
-
-function hitBomb (player, bomb)
-{
-    this.physics.pause();
-    
-    player.setTint(0xff0000);
-    
-    player.anims.play('turn');
-    
-    gameOver = true;
 }
 
 function hitbox (player, box)
 {
     box.disableBody(true, true);
     
-    //  Add and update the score
     score += 10;
     $('#score').html("Score : "+score);
-    //scoreText.setText('Score: ' + score);
 }
 
 var SceneA = new Phaser.Class({
@@ -64,7 +45,6 @@ var SceneA = new Phaser.Class({
         this.load.image('overlay', 'assets/backgroundb3.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
         this.load.image('titre', 'assets/titre.png');
         this.load.image('boxS', 'assets/boxS.png');
         this.load.image('boxM', 'assets/boxM.png');
@@ -210,9 +190,7 @@ var SceneA = new Phaser.Class({
             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
             
         });
-        
-        bombs = this.physics.add.group();
-        
+                
         box = this.physics.add.group();
 
         mobs = {
@@ -228,7 +206,6 @@ var SceneA = new Phaser.Class({
         this.physics.add.collider(player, mobs);
         this.physics.add.collider(stars, platforms);
         this.physics.add.collider(box, platforms);
-        this.physics.add.collider(bombs, platforms);
         Object.keys(mobs).forEach((mobtype)=>{
             var group = mobs[mobtype];
             this.physics.add.collider(group, platforms);
@@ -237,25 +214,10 @@ var SceneA = new Phaser.Class({
         //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
         this.physics.add.overlap(player, stars, collectStar, null, this);
         this.physics.add.overlap(player, box, hitbox, null, this);
-        this.physics.add.collider(player, mobs, hitBomb, null, this);
+        this.physics.add.collider(player, mobs, null, this);
         
         // Activation du suivi du joueur par la caméra
         this.cameras.main.startFollow(player);
-
-
-        //Aidé par Laure
-        let temp = this;
-        // for(var i=0; i<liste_plateformes.length;i++) {
-        //     if(Math.random()<=2/3) {
-        //         var mob = this.physics.add.sprite(liste_plateformes[i].x, 50, 'cerf');
-        //         mobs.add(mob);
-        //         mob.depart=liste_plateformes[i].x;
-        //         mob.limite = 150;
-        //         mob.setCollideWorldBounds(true); //rebond sur les bords du monde
-        //         mob.allowGravity = true;
-        //         mob.setVelocityX(120);
-        //     }
-        // }
 
         for(var i=0; i<liste_mobs.length;i++) {
                 var mob_param = liste_mobs[i];
@@ -271,17 +233,6 @@ var SceneA = new Phaser.Class({
                 mob.allowGravity = true;
                 mob.setVelocityX(120);
         }
-
-        // for(var i=600; i<4000; i+=800) {
-        //     var mob = this.physics.add.sprite(i, 440, 'cerf');
-        //     mobs.add(mob);
-        //     mob.depart=i;
-        //     mob.limite = 250;
-        //     mob.setCollideWorldBounds(true); //rebond sur les bords du monde
-        //     mob.allowGravity = true;
-        //     mob.setVelocityX(120);
-
-        // }
     },
     
     update: function ()
@@ -315,7 +266,7 @@ var SceneA = new Phaser.Class({
             player.setVelocityY(-330);
         }
 
-        //Aidé par Laure
+        //Merci Laure
         Object.keys(mobs).forEach((mobtype)=>{
             var group = mobs[mobtype];
             group.children.iterate(function(mob) {
@@ -333,223 +284,6 @@ var SceneA = new Phaser.Class({
     }
 
 });
-
-// var SceneB = new Phaser.Class({
-    
-//     Extends: Phaser.Scene,
-    
-//     initialize:
-    
-//     function SceneB ()
-//     {
-//         Phaser.Scene.call(this, { key: 'sceneB' });
-//     },
-    
-//     preload: function ()
-//     {
-//         this.load.image('overlay', 'assets/backgroundb.jpg');
-//         this.load.image('sky', 'assets/sky.png');
-//         this.load.image('ground', 'assets/platform.png');
-//         this.load.image('star', 'assets/star.png');
-//         this.load.image('bomb', 'assets/bomb.png');
-//         this.load.image('titre', 'assets/titre.png');
-//         this.load.image('boxS', 'assets/boxS.png');
-//         this.load.image('boxM', 'assets/boxM.png');
-//         this.load.image('boxL', 'assets/boxL.png');
-//         this.load.image('boxBS', 'assets/boxBS.png');
-//         this.load.image('boxBS2', 'assets/boxBS2.png');
-//         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 48, frameHeight: 46 });
-//         this.load.image('mountL', 'assets/montagneL2.png');
-//         this.load.image('mountM', 'assets/montagneM2.png');
-//         this.load.image('mountS', 'assets/montagneS2.png');
-//         this.load.image('nuageS', 'assets/nuageS.png');
-//         this.load.image('nuageM', 'assets/nuageM2.png');
-//         this.load.image('buissonL', 'assets/buissonL2.png');
-//         this.load.image('buissonM', 'assets/buissonM2.png');
-//         this.load.image('buissonS', 'assets/buissonS2.png');
-//         this.load.image('arbreM', 'assets/arbreM2.png');
-//         this.load.image('arbreS', 'assets/arbreS2.png');
-//         this.load.image('arbreL', 'assets/arbreL2.png');
-//         this.load.image('groundL', 'assets/groundL.png');
-//         this.load.image('groundM', 'assets/groundM.png');
-//         this.load.image('groundSG', 'assets/groundSG.png');
-//         this.load.image('groundSD', 'assets/groundSD.png');
-//     },
-    
-//     create: function ()
-//     {
-//         // Création de la camera et agrandissement de la zone de jeu
-//         this.cameras.main.setBounds(0,0,8000,600);
-//         this.physics.world.setBounds(0,0,8000,600);
-        
-//         //  A simple background for our game avec mise à l'échelle pour le 1600x600
-//         this.add.image(4000, 285, 'overlay').setScale(1.07,1);
-        
-//         //  The platforms group contains the ground and the 2 ledges we can jump on
-//         platforms = this.physics.add.staticGroup();
-//         background = this.physics.add.staticGroup();
-        
-//         //  Here we create the ground.
-//         //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-//         //platforms.create(2000, 568, 'groundM').setScale(32,2).refreshBody();
-        
-//         //  Now let's create some ledges
-//         liste_plateformes.forEach(function(p, i, array) {
-//             platforms.create(p.x, p.y, p.t).setScale(p.sx, p.sy).refreshBody();
-//         });
-        
-//         //pour les boites
-//         /*var physics = this.physics;
-//         liste_boites.forEach(function(p, i, array) {
-//             var box = physics.add.sprite(p.x, p.y, p.t);
-//             physics.add.collider(box, platforms);
-//         });*/
-        
-//         //  Pour les éléments de fond
-//         liste_assets.forEach(function(p, i, array) {
-//             background.create(p.x, p.y, p.t).setScale(p.sx, p.sy).refreshBody();
-//         });
-        
-//         // The player and its settings
-//         player = this.physics.add.sprite(100, 450, 'dude');
-        
-//         player.setSize(25, 40, true);
-        
-//         //  Player physics properties. Give the little guy a slight bounce.
-        
-//         player.setBounce(0.2);
-//         player.setCollideWorldBounds(true);
-//         //player.setSize(realWidth-5, realHeight-5, false).setOffset(2,2);
-//         //player.setGravityY(100);
-        
-//         //  Our player animations, turning, walking left and walking right.
-//         this.anims.create({
-//             key: 'left',
-//             frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-//             frameRate: 10,
-//             repeat: -1
-//         });
-        
-//         this.anims.create({
-//             key: 'turn',
-//             frames: [ { key: 'dude', frame: 4 } ],
-//             frameRate: 20
-//         });
-        
-//         this.anims.create({
-//             key: 'right',
-//             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-//             frameRate: 10,
-//             repeat: -1
-//         });
-        
-//         //  Input Events
-//         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-//         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-//         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-//         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        
-//         //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-//         stars = this.physics.add.group({
-//             key: 'star',
-//             repeat: 22,
-//             setXY: { x: 12, y: 0, stepX: 70 }
-//         });
-        
-//         stars.children.iterate(function (child) {
-            
-//             //  Give each star a slightly different bounce
-//             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-            
-//         });
-        
-//         bombs = this.physics.add.group();
-        
-//         box = this.physics.add.group();
-        
-//         //  The score
-//         //scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-        
-//         //  Collide the player and the stars with the platforms
-//         this.physics.add.collider(player, platforms);
-//         this.physics.add.collider(player, mobs);
-//         this.physics.add.collider(stars, platforms);
-//         this.physics.add.collider(box, platforms);
-//         this.physics.add.collider(bombs, platforms);
-        
-//         //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-//         this.physics.add.overlap(player, stars, collectStar, null, this);
-//         this.physics.add.overlap(player, box, hitbox, null, this);
-//         this.physics.add.collider(player, mobs, hitBomb, null, this);
-        
-//         // Activation du suivi du joueur par la caméra
-//         this.cameras.main.startFollow(player);
-//     },
-    
-//     update: function ()
-//     {
-//         if (gameOver)
-//         {
-//             return;
-//         }
-        
-//         if (keyLEFT.isDown)
-//         {
-//             player.setVelocityX(-160);
-            
-//             player.anims.play('left', true);
-//         }
-//         else if (keyRIGHT.isDown)
-//         {
-//             player.setVelocityX(160);
-            
-//             player.anims.play('right', true);
-//         }
-//         else
-//         {
-//             player.setVelocityX(0);
-            
-//             player.anims.play('turn');
-//         }
-        
-//         if (keyUP.isDown && player.body.touching.down)
-//         {
-//             player.setVelocityY(-330);
-//         }
-//     }
-    
-// });
-
-// var SceneC = new Phaser.Class({
-    
-//     Extends: Phaser.Scene,
-    
-//     initialize:
-    
-//     function SceneC ()
-//     {
-//         Phaser.Scene.call(this, { key: 'sceneC' });
-//     },
-    
-//     preload: function ()
-//     {
-//         this.load.image('mech', 'assets/titre.png');
-//     },
-    
-//     create: function ()
-//     {
-//         this.add.sprite(Phaser.Math.Between(0, 800), 300, 'mech');
-        
-//         this.input.once('pointerdown', function (event) {
-            
-//             console.log('From SceneC to SceneA');
-            
-//             this.scene.start('sceneA');
-            
-//         }, this);
-//     }
-    
-// });
 
 var config = {
     type: Phaser.AUTO,
@@ -573,7 +307,6 @@ var A = 32;
 var player;
 var stars;
 var box;
-var bombs;
 var platforms;
 var background;
 var cursors;
@@ -586,7 +319,7 @@ var animations_mobs = {
     'porc': {'right': 'pright', 'left': 'pleft'},
 };
 var liste_mobs = [
-    {platform: 3, type: 'porc'},
+    {platform: 1, type: 'porc'},
     {platform: 5, type: 'porc'},
     {platform: 7, type: 'porc'},
     {platform: 13, type: 'cerf'},
